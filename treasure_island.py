6ca8ef0b7d35e0b9d651f4b565c24fa40e222c75
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-07-12 18:15:44 -0400

    complete conditionals

---
+++ treasure_island.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+choice_one = input("Will you go left or right?: ")
+
+choice_two = input("Will you swim or wait?: ")
+
+choice_three = input("Which door will you choose?: ")
+
+if choice_one == "left":
+    if choice_two =="wait":
+        if choice_three =="yellow":
+            print("You Win!")
+        elif choice_three =="red":
+            print("Yike! You were just burned by fire. Game over :(")
+        elif choice_three =="blue":
+            print("Welp. You were just eaten bt beasts. Game over :(")
+        else:
+            print("Game Over")
+    else:
+        print("You were just eaten by a trout! Game over!")
+else:
+    print("You were captured by a trap setup by the lizard folk. Game over.")
